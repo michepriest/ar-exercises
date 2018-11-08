e643736f413e6112bdd114f1c195785ae9be6364
michepriest <miche.priest@gmail.com>
2018-11-08 05:02:28 +0000

    add many employees to one store

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,12 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Vaz", last_name: "Allen", hourly_rate: 60)
+@store1.employees.create(first_name: "Jeremy", last_name: "Holman", hourly_rate: 60)
+@store1.employees.create(first_name: "Nima", last_name: "Boscarino", hourly_rate: 60)
+@store1.employees.create(first_name: "Sam", last_name: "Meech-Ward", hourly_rate: 60)
+@store2.employees.create(first_name: "Rebecca", last_name: "Haliburton", hourly_rate: 60)
+@store2.employees.create(first_name: "Joel", last_name: "Shinness", hourly_rate: 60)
+@store2.employees.create(first_name: "Mario", last_name: "Viens", hourly_rate: 60)
+@store2.employees.create(first_name: "Dane", last_name: "Sirois", hourly_rate: 60)
